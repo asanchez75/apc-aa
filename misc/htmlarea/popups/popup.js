f6221b517cfaa9a17df28a37bb4384ace524c4a1
honzam <honzam@feb4f9ab-8419-0410-81fa-d11e1fc4b9d5>
2004-11-07 20:52:20 +0000

    new version of htmlarea (3.0rc1)

--- misc/htmlarea/popups/popup.js
+++ misc/htmlarea/popups/popup.js
@@ -1,3 +1,16 @@
+// htmlArea v3.0 - Copyright (c) 2002, 2003 interactivetools.com, inc.
+// This copyright notice MUST stay intact for use (see license.txt).
+//
+// Portions (c) dynarch.com, 2003
+//
+// A free WYSIWYG editor replacement for <textarea> fields.
+// For full source code and docs, visit http://www.interactivetools.com/
+//
+// Version 3.0 developed by Mihai Bazon.
+//   http://dynarch.com/mishoo
+//
+// $Id$
+
 function getAbsolutePos(el) {
 	var r = { x: el.offsetLeft, y: el.offsetTop };
 	if (el.offsetParent) {
@@ -6,6 +19,15 @@ function getAbsolutePos(el) {
 		r.y += tmp.y;
 	}
 	return r;
+};
+
+function comboSelectValue(c, val) {
+	var ops = c.getElementsByTagName("option");
+	for (var i = ops.length; --i >= 0;) {
+		var op = ops[i];
+		op.selected = (op.value == val);
+	}
+	c.value = val;
 };
 
 function __dlg_onclose() {
@@ -31,12 +53,12 @@ function __dlg_init(bottom) {
 					// only if we call it twice we get the
 					// correct size.
 		window.addEventListener("unload", __dlg_onclose, true);
+		window.innerWidth = body.offsetWidth + 5;
+		window.innerHeight = body_height + 2;
 		// center on parent
 		var x = opener.screenX + (opener.outerWidth - window.outerWidth) / 2;
 		var y = opener.screenY + (opener.outerHeight - window.outerHeight) / 2;
 		window.moveTo(x, y);
-		window.innerWidth = body.offsetWidth + 5;
-		window.innerHeight = body_height;
 	} else {
 		// window.dialogHeight = body.offsetHeight + 50 + "px";
 		// window.dialogWidth = body.offsetWidth + "px";
@@ -50,19 +72,28 @@ function __dlg_init(bottom) {
 		var y = (screen.availHeight - H) / 2;
 		window.moveTo(x, y);
 	}
+	document.body.onkeypress = __dlg_close_on_esc;
 };
 
-function __dlg_translate(namespace) {
-	var spans = document.getElementsByTagName("span");
-	for (var i = spans.length; --i >= 0;) {
-		var span = spans[i];
-		if (span.firstChild && span.firstChild.data) {
-			var txt = namespace.I18N[span.firstChild.data];
-			if (txt)
-				span.firstChild.data = txt;
+function __dlg_translate(i18n) {
+	var types = ["input", "select", "legend", "span", "option", "td", "button", "div"];
+	for (var type = 0; type < types.length; ++type) {
+		var spans = document.getElementsByTagName(types[type]);
+		for (var i = spans.length; --i >= 0;) {
+			var span = spans[i];
+			if (span.firstChild && span.firstChild.data) {
+				var txt = i18n[span.firstChild.data];
+				if (txt)
+					span.firstChild.data = txt;
+			}
+                        if (span.title) {
+				var txt = i18n[span.title];
+				if (txt)
+					span.title = txt;
+                        }
 		}
 	}
-	var txt = namespace.I18N[document.title];
+	var txt = i18n[document.title];
 	if (txt)
 		document.title = txt;
 };
@@ -71,4 +102,13 @@ function __dlg_translate(namespace) {
 function __dlg_close(val) {
 	opener.Dialog._return(val);
 	window.close();
+};
+
+function __dlg_close_on_esc(ev) {
+	ev || (ev = window.event);
+	if (ev.keyCode == 27) {
+		window.close();
+		return false;
+	}
+	return true;
 };
