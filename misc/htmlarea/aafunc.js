5a93afe0bc680056c0af381e9e622d4f56081ca5
honzam <honzam@feb4f9ab-8419-0410-81fa-d11e1fc4b9d5>
2004-11-12 16:05:04 +0000

    fixed HTMLArea 3.0rc1 support

--- misc/htmlarea/aafunc.js
+++ misc/htmlarea/aafunc.js
@@ -2,15 +2,20 @@
 
 
     function switchHTML(name) {
-        elem = document.inputform.eval(name+"html");
-        for (i=0; i<elem.length; i++) {
-            if (elem[i].value == "h") {
-                elem[i].checked = true;
+        elem = document.getElementById(name+"html");
+        //elem = document.inputform.eval(name+"html");
+        if ( elem != null ) {
+            for (i=0; i<elem.length; i++) {
+                if (elem[i].value == "h") {
+                    elem[i].checked = true;
+                }
             }
         }
         if (HTMLArea.checkSupportedBrowser) {
-            elem = document.getElementById("htmlplainspan");
-            elem.style.display = "none";
+            elem = document.getElementById("htmlplainspan"+name);
+            if ( elem != null ) {
+                elem.style.display = "none";
+            }
         }
     }
 
@@ -31,19 +36,19 @@
         config.width = eval(cols*12)+"px";
         if (tableop == true) area.registerPlugin("TableOperations");
         if (spell == true) area.registerPlugin("SpellChecker");
-        area.registerPlugin("InsertFile");
+//        area.registerPlugin("InsertFile");
         area.generate();
         switchHTML(name);
         return false;
     }
 
     function openHTMLAreaFullscreen(name, session) { // open HTMLArea in popupwindow
-        htmlArea = new HTMLArea(name); // create dummy HTMLArea object
-        htmlArea.session = session;
-        htmlArea._textArea = document.getElementById(name); // set textarea name
-        HTMLArea._object = htmlArea; // HTMLArea object is used in popupwindow
+        ha = new HTMLArea(name); // create dummy HTMLArea object
+        HTMLArea.session = session;
+        ha._textArea = document.getElementById(name); // set textarea name
+        HTMLArea._object = ha; // HTMLArea object is used in popupwindow
         HTMLArea._object.isnormal = "1"; // parent area is normal textarea
-        if (htmlArea.is_ie) { // different window opening for IE and other browsers
+        if (HTMLArea.is_ie) { // different window opening for IE and other browsers
             window.open(long_editor_url+"popups/fullscreen.html", "ha_fullscreen",
                         "toolbar=no,location=no,directories=no,status=no,menubar=no," +
                         "scrollbars=no,resizable=yes,width=640,height=480");
