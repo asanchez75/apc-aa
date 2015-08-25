b503f15f8ca443ba155146621658683d1c6d4266
honzam <honzam@feb4f9ab-8419-0410-81fa-d11e1fc4b9d5>
2015-08-25 15:33:42 +0000

    CKEditor used as WYSIWYG editor insted of Xinha

--- trunk/misc/htmlarea/modules/FullScreen/lang/cz.js
+++
@@ -1,6 +0,0 @@
-// I18N constants
-// LANG: "cz", ENCODING: UTF-8
-// translated: Jan Malík, honza.malik@ecn.cz, http://www.econnect.cz
-{
-  "Maximize/Minimize Editor": "Maximalizovat/minimalizovat editor"
-};
