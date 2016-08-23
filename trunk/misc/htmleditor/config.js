9068e70ff437c864983fad7da7270a4389f560ea
honzam <honzam@feb4f9ab-8419-0410-81fa-d11e1fc4b9d5>
2016-08-23 07:13:46 +0000

    CKeditor upgraded to 4.5.10; allowed justify, undo; content filter is turn off

--- trunk/misc/htmleditor/config.js
+++ trunk/misc/htmleditor/config.js
@@ -1,7 +1,9 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
+
+// when upgrading, keep this file - all others you can delete/replace. Honza
 
 CKEDITOR.editorConfig = function( config ) {
     // Define changes to default configuration here.
@@ -28,16 +30,25 @@ CKEDITOR.editorConfig = function( config ) {
 
     // Remove some buttons provided by the standard plugins, which are
     // not needed in the Standard(s) toolbar.
-    config.removeButtons = 'Underline,Subscript,Superscript';
+    config.removeButtons = 'Underline,Subscript,Superscript,JustifyLeft,JustifyBlock,Font,About';
+    // config.removeButtons = 'Underline,Subscript,Superscript,Font,About';   // Honza Change
 
     // Set the most common block elements.
-    config.format_tags = 'p;h1;h2;h3;pre';
+    //config.format_tags = 'p;h1;h2;h3;pre';
+    config.format_tags = 'p;h1;h2;h3;h4;h5;h6;pre';  // Honza Change
 
     // Simplify the dialog windows.
-    config.removeDialogTabs = 'image:advanced;link:advanced';
+    //config.removeDialogTabs = 'image:advanced;link:advanced'; // Honza Change
 
     // Honza's changes
     config.entities = false;
+    //config.extraAllowedContent = 'span;*[id];a[rel];*(*);*{*}'; // any class and any inline style...
+    config.allowedContent = true;   // disable filtering at all - toto je poslední možnost
+
+    // allow i tags to be empty (for font awesome)
+    CKEDITOR.dtd.$removeEmpty['i'] = false
+
+    config.embed_provider = '//iframe.ly/api/oembed?url={url}&callback={callback}&api_key=31993d559460afddc115dc';
 
     config.filebrowserBrowseUrl      = _aa_url + 'misc/filebrowser/browse.php?opener=ckeditor&type=files';
     config.filebrowserImageBrowseUrl = _aa_url + 'misc/filebrowser/browse.php?opener=ckeditor&type=images';
