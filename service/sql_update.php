8faa3f4c18a93c68e1c70657d6ca1006e7846c78
honzam <honzam@feb4f9ab-8419-0410-81fa-d11e1fc4b9d5>
2016-11-02 10:31:21 +0000

    the sql_update.php is more secure

--- service/sql_update.php
+++ service/sql_update.php
@@ -1,5 +1,5 @@
 <?php
-//$Id: sql_update.php3 2683 2008-09-26 12:00:42Z honzam $
+//$Id: sql_update.php 2683 2008-09-26 12:00:42Z honzam $
 /*
 Copyright (C) 1999, 2000 Association for Progressive Communications
 http://www.apc.org/
@@ -18,19 +18,103 @@ http://www.apc.org/
     along with this program (LICENSE); if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
-// my change
-
 // script for MySQL database update
 
 // this script updates the database to last structure, create all tables, ...
 // can be used for upgrade from apc-aa v. >= 1.5 or for create new database
 
+/** FILL SOME ACCESS CODE HERE !!!
+ *  example:
+ *     define('ACCESS_CODE','de46272*_!%@*dhje-rty362ddj');
+ *  (but use your own code)
+ *  Then fill the same code to the field Access Code field on the page to access
+ *  this script. The code must be 10 character long at least.
+ *
+ *  It is recommended to delete the access code after you finish your setup, to
+ *  disable access code guessing from attackers.
+ */
+define('ACCESS_CODE','de46272*_!%@*dhje-rty362ddj');
+
+
+/** INSTRUCTIONS BEFORE USE
+ *  This script is written to be not destructive. It updates the database to
+ *  the latest structure, create all tables, ...
+ *  It can be used for upgrade from previous versions of apc-aa or for creating
+ *  new database.
+ *  It creates temporary tables first, then copies data from old tables to the
+ *  temporary ones (tmp_*) and after successfull copy it drops old tables and
+ *  renames temporary ones to right names. Then it possibly updates common
+ *  records (like default field definitions, module templates, constants and
+ *  templates).</p>
+ *
+ *  However, it is strongly recommended backup your current database !!!
+ *  Use something like:
+ *     mysqldump --lock-tables -h DB_HOST -u DB_USER -p --opt DB_NAME > ./aadbbackup.sql
+ *  (replace DB_HOST, DB_USER and B_NAME with values from your config.php3 file)
+ */
+
 
 ini_set('display_errors', 'On');
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
- #crash!
+// sleep one second to make any brute force attacks harder
+sleep(1);
+
+
+if (!$_GET['silent']) {
+    echo '
+      <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+      <html>
+      <head>
+        <title>APC-AA database update script</title>
+      </head>
+      <body>
+      <h1>ActionApps database update</h1>
+      <p>This script is written to be not destructive. It creates temporary tables
+         first, then copies data from old tables to the temporary ones (tmp_*) and
+         after successfull copy it drops old tables and renames temporary ones to
+         right names. Then it possibly updates common records (like default field
+         definitions, module templates, constants and templates).</p>
+      <p style="color:red;">However, it is strongly recommended backup your current
+      database !!!</p>
+      <p>See instructions directly in this file (sql_update.php) on the server.</p>';
+}
+
+if ( !constant('ACCESS_CODE') OR (strlen(constant('ACCESS_CODE'))<10) ) {
+    echo '<p>For update or restore you need to edit this script on the server
+          (sql_update.php) and fill there some Access Code which you then fill
+          also here (it is for security reasons).</p>
+          <p>Just refresh this page after you fill the Access code to the file.</p>
+          </body>
+          </html>';
+    exit;
+}
+
+if (!is_callable('myspecialchars')) {
+    function myspecialchars( $var, $double_encode=true) {
+        return htmlspecialchars( $var, ENT_COMPAT | ENT_HTML401, 'ISO-8859-1', $double_encode);
+    }
+}
+
+if (!$_GET['silent']) {
+    echo '<form name="f" action="' .$_SERVER['PHP_SELF'] .'">
+            Access Code
+            <input type="password" name="acccode" value="'.myspecialchars($_GET['acccode']).'"><br>
+
+            Write to database <input type="checkbox" name="fire" value="1"'.($_GET['fire'] ? ' checked' : ''). '"><br>
+            <small>Check this for real work with writing to database</small>
+            <br><br>
+
+            <input type="submit" name="dotest" value="Test">
+            <input type="submit" name="update" value="Install / Update">
+            <input type="submit" name="restore" value="Restore">
+          </form>';
+}
+
+if ( (strlen($_GET['acccode'])<10) OR ($_GET['acccode']!=constant('ACCESS_CODE')) ) {
+    echo '</body></html>';
+    exit;
+}
 
 
 /**
@@ -163,13 +247,6 @@ if (!is_callable('quote')) {
     }
 }
 
-if (!is_callable('myspecialchars')) {
-    function myspecialchars( $var, $double_encode=true) {
-        return htmlspecialchars( $var, ENT_COMPAT | ENT_HTML401, 'ISO-8859-1', $double_encode);
-    }
-}
-
-
 
 // do not reorder those requires because of metabase and varset dependency
 require_once dirname(__FILE__)."/metabase.class.php3";
@@ -273,74 +350,28 @@ class AA_SQL_Updater {
 
 }
 
-
-if (!$_GET['silent']) {
-    echo '
-      <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-      <html>
-      <head>
-        <title>APC-AA database update script</title>
-      </head>
-      <body>
-      <h1>ActionApps database update</h1>
-      <p>This script is written to be not destructive. It creates temporary tables
-         first, then copies data from old tables to the temporary ones (tmp_*) and
-         after successfull copy it drops old tables and renames temporary ones to
-         right names. Then it possibly updates common records (like default field
-         definitions, module templates, constants and templates).</p>
-      <p><font color="red">However, it is strongly recommended backup your current
-      database !!!</font><br><br>Something like:<br><code>mysqldump --lock-tables -h '.DB_HOST.' -u '.DB_USER.' -p --opt '.DB_NAME.' &gt; ./'.DB_NAME.'_'.date('ymd').'.sql</code></p>
-
-      <form name="f" action="' .$_SERVER['PHP_SELF'] .'">
-      ';
-}
-
-
 $updater = new AA_SQL_Updater();
 
 if ($_GET['update']) {
-    if ( substr( DB_PASSWORD, 0, 5 ) != $_GET['dbpw5'] ) {
-        echo 'Bad password. Please fill first five characters from aa database password in the field bellow';
-    } else {
-        if (!$_GET['fire']) {
-            AA_Optimize::justPrint(true);
-        }
-        $status = $updater->update();
-        echo ($status ? 'OK ' : 'Err ') . $updater->report();
+    if (!$_GET['fire']) {
+        AA_Optimize::justPrint(true);
     }
+    $status = $updater->update();
+    echo ($status ? 'OK ' : 'Err ') . $updater->report();
 }
 elseif ( $_GET['restore']) {
-    if ( substr( DB_PASSWORD, 0, 5 ) != $_GET['dbpw5'] ) {
-        echo 'Bad password. Please fill first five characters from aa database password in the field bellow';
-    } else {
-        if (!$_GET['fire']) {
-            AA_Optimize::justPrint(true);
-        }
-        $status = $updater->restore();
-        echo ($status ? 'OK ' : 'Err ') . $updater->report();
+    if (!$_GET['fire']) {
+        AA_Optimize::justPrint(true);
     }
+    $status = $updater->restore();
+    echo ($status ? 'OK ' : 'Err ') . $updater->report();
 }
 elseif ( $_GET['dotest']) {
-
     $status = $updater->test();
     echo ($status ? 'OK ' : 'Err ') . $updater->report();
 }
 
-
 if (!$_GET['silent']) {
-    echo '  For update or restore you need to know database password (see DB_PASSWORD in config.php3 file) - it is from security reasons. <br><br>
-            Fill in first five characters of the password here
-
-            <input type="text" name="dbpw5" size="5" maxsize="5" value="'.$_GET['dbpw5'].'"><br>
-            Write to database <input type="checkbox" name="fire" value="1"'.($_GET['fire'] ? ' checked' : ''). '"><br>
-            <small>Check this for real work with writing to database</small>
-            <br><br>
-
-            <input type="submit" name="dotest" value="Test">
-            <input type="submit" name="update" value="Install / Update">
-            <input type="submit" name="restore" value="Restore">
-           </form>
-          </body>
-          </html>';
+    echo '</body></html>';
 }
 ?>
